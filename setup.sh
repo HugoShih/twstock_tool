aaf0f3b44821d1dd77262f58deebccede0a297cc
HugoShih <hugo@postcolour.com>
2020-09-08 18:10:38 +0800

    add heroku deploy files

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"hugo@postcolour.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
